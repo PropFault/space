f2b3c26b5ba719deda2f7a749cae35e3a02843c5
Benedikt Brunner <benedikt.brunner@protonmail.com>
2022-05-26 13:34:20 +0200

    up

--- TextureToPolygon.gd
+++ TextureToPolygon.gd
@@ -23,21 +23,41 @@ func randomWalk(x, y, image):
 		cursorX = clamp(cursorX +min(rng.randi_range(-1,3),1), 0, width-1)
 		readPixel = image.get_pixel(cursorX, cursorY)
 	image.unlock()
-	return Vector2(cursorX, cursorY)
+	return Vector2(cursorX - width/2, cursorY - height/2)
 func generate():
 	var image = tex.get_data()
 	var width = image.get_width()
 	var height = image.get_height()
 	points = []
-	for x in range(0, width-1):
+	for x in range(0, width-1,2):
 		points.append(randomWalk(x,0,image))
-	for y in range(0, height-1):
+	for y in range(0, height-1,2):
 		points.append(randomWalk(0,y,image))
-	for x in range(0, width-1):
+	for x in range(0, width-1,2):
 		points.append(randomWalk(x,height-1,image))
-	for y in range(0, height-1):
+	for y in range(0, height-1,2):
 		points.append(randomWalk(width-1,y,image))
-	points.append(points[0])
+	var first = points[0]
+	var i = 0
+	while(i < points.size()):
+		var j = 0
+		while(j < points.size()):
+			if(points[i] == points[j] and i != j):
+				points.remove(j)
+			else:
+				j = j+1
+		i = i + 1
+	# sort points by winding
+	var woundPoints = []
+	var origin = Vector2(width/2.0, height/2.0)
+	var angle = 0
+	var base = origin + Vector2(width/2, 0)
+	var step = PI / 1000
+	while(angle < 2*PI):
+		var point = Vector2(base.x * cos(angle) - base.y * sin(angle),
+							base.y * cos(angle) + base.x * sin(angle))
+		draw_line(origin, point, Color.red)
+		angle = angle + step
 	self.polygon = points
 
 # Called when the node enters the scene tree for the first time.
@@ -48,8 +68,18 @@ func _ready():
 func _process(delta):
 	if(self.polygon.size() == 0):
 		generate()
-		
 
-func _draw():
-	for point in points:
-		draw_circle(point, 1, Color.red)
+#func _draw():
+#	
+#	var woundPoints = []
+#	var origin = Vector2(0,0)
+#	var angle = 0
+#	var base = Vector2(32/2.0,0)
+#	var step = PI / 1000
+#	while(angle < 2*PI):
+#		var point = Vector2(base.x * cos(angle) - base.y * sin(angle),
+#							base.y * cos(angle) + base.x * sin(angle))
+#		
+#		draw_line(origin, point, Color.red)
+#		angle = angle + step
+#
